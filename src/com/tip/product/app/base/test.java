ceee3cfd4f9c13e0256bfb1bc62323eaf9e9db66
chuchao222@qq.com <chuc@BITAUTO-K36G4RT.yixin.dk>
2016-08-08 17:34:25 +0800

    测试

---
+++ src/com/tip/product/app/base/test.java
@@ -0,0 +1,13 @@
+package com.tip.product.app.base;
+
+public class test {
+
+	public test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+	}
+}
+
