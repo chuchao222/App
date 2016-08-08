79c0c40193610024ac84a31a1ad3a06c81ded940
chuchao222@qq.com <chuc@BITAUTO-K36G4RT.yixin.dk>
2016-08-08 17:43:00 +0800

    test2

--- src/com/tip/product/app/base/test.java
+++ src/com/tip/product/app/base/test.java
@@ -1,11 +1,19 @@
 package com.tip.product.app.base;
-
+/**
+ * 
+ * Package : com.tip.product.app.base
+ * 
+ * @author YixinCapital -- chuc
+ *		   2016年8月8日 下午5:42:23
+ *
+ */
 public class test {
 
 	public test() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
+	
 	public static void main(String[] args) {
 		System.out.println("hello world!");
 	}
